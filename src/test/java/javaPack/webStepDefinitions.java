927f631f56ad845bb7b0856ff5a3c9106677d75a
jwwood87 <jwwood87@gmail.com>
2016-12-14 16:24:28 -0700

    Variablized a method to support any uri

--- src/test/java/javaPack/webStepDefinitions.java
+++ src/test/java/javaPack/webStepDefinitions.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertTrue;
 public class webStepDefinitions {
 
     WebDriver driver;
+
 //    @BeforeClass
 //    public void setup () {
 //        System.out.println("\n" + "Hi, from within the Before setup method.");
@@ -41,63 +42,23 @@ public class webStepDefinitions {
         driver.manage().window().maximize();
     }
 
-    @Test
-    public void searchGoogle() {
-
-// Note: the pattern is case sensitive
-// Todo: make the pattern check so that it is not case sensitive
-        String searchPattern = "Ugly Code";
-
-        // Launch browser to Google and enter search condition
-        driver.get("http://www.google.com");
-        System.out.println("The Firefox Browser is on " + driver.getTitle() + "\n");
-        WebElement searchWindow = driver.findElement(By.id("lst-ib"));
-        searchWindow.sendKeys(searchPattern);
-        searchWindow.submit();
-        WebElement searchButton = driver.findElement(By.className("sbico-c"));
-        searchButton.click();
-        // Check whether the search results include the search pattern for each of the first 4 returns
-        WebElement searchResult1 = driver.findElement(By.xpath("//div[@id='rso']/div[1]/div/div[1]/div/h3/a"));
-        System.out.println( "The first search return is \"" + searchResult1.getText() + "\"" );
-        if (returnIncludesSearch(searchPattern,searchResult1.getText())) {
-            System.out.println("The 1st result includes the pattern " + "\n");
-        } else
-            System.out.println("The 1st result does not include the pattern" + "\n");
-
-        WebElement searchResult2 = driver.findElement(By.xpath("//div[@id='rso']/div[1]/div/div[2]/div/h3/a"));
-        System.out.println( "The 2nd search return is \"" + searchResult2.getText() + "\"" );
-        if (returnIncludesSearch(searchPattern,searchResult2.getText())) {
-            System.out.println("The 2nd result includes the pattern " + "\n");
-        } else
-            System.out.println("The 2nd result does not include the pattern" + "\n");
-
-        WebElement searchResult3 = driver.findElement(By.xpath("//div[@id='rso']/div[1]/div/div[3]/div/h3/a"));
-        System.out.println( "The third search return is \"" + searchResult3.getText() + "\"" );
-        if (returnIncludesSearch(searchPattern,searchResult3.getText())) {
-            System.out.println("The 3rd result includes the pattern." + "\n");
-        } else
-            System.out.println("The 3rd result does not include the pattern." + "\n");
-
-        WebElement searchResult4 = driver.findElement(By.xpath("//div[@id='rso']/div[1]/div/div[4]/div/h3/a"));
-        System.out.println( "The fourth search return is \"" + searchResult4.getText() + "\"" );
-        if (returnIncludesSearch(searchPattern,searchResult4.getText())) {
-            System.out.println("The 4th result includes the pattern." + "\n");
-        } else
-            System.out.println("The 4th result does not include the pattern." + "\n");
-    }
-
-    @Given("^I navigate to Google$")
-    public void i_navigate_to_Google() throws  Throwable{
+    @Given("^I launch a browser$")
+    public void i_launch_a_browser() throws Throwable {
         // Launch browser and navigate to Google's main web page
         driver = new FirefoxDriver();
         System.setProperty("webdriver.gecko.driver","C:\\Program Files\\Selenium\\geckodriver.exe");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://www.google.com");
     }
 
-    @When("^I search for \"([^\"]*)\" and get the expected results$")
-    public void i_search_for(String searchPattern) throws Throwable {
+    @Given("^I navigate to \"([^\"]*)\"")
+    public void i_navigate_to_Google(String uri) throws  Throwable{
+
+        driver.get(uri);
+    }
+
+    @When("^I search Google with the pattern, \"([^\"]*)\", and the results include the pattern$")
+    public void i_search_for(String searchPattern) {
 
         boolean searchResultPresent = false;
         // Enter the designated search condition into Google's page
@@ -138,14 +99,12 @@ public class webStepDefinitions {
             searchResultPresent=true;
         } else
             System.out.println("The 4th result does not include the pattern." + "\n");
-        assertTrue(searchResultPresent);
-
-    }
-
-
-    @Then("^I get the results I expect$")
-    public void i_get_the_results_I_expect() throws Throwable {
-        System.out.println("I'm getting the results I expect, dude.");
+        if (searchResultPresent==true)
+            assertTrue(true);
+        else {
+            driver.quit();
+            assertTrue(searchResultPresent);
+        }
     }
 
     @Then("^I close the browser$")
--- src/test/resources/sample2.feature
+++ src/test/resources/sample2.feature
@@ -21,12 +21,25 @@ Feature: This feature is my second cucumber test
   Scenario Outline: This is my test to support Keith's request
     Keith asked me to write code for a Google Search
 
-    Given I navigate to Google
-    #Todo Need to implement an assertion to test whether we really get expected results
-    When I search for "<SearchPattern>" and get the expected results
+    Given I launch a browser
+    And I navigate to "http://google.com"
+    When I search Google with the pattern, "<SearchPattern>", and the results include the pattern
     Then I close the browser
     Examples:
       | SearchPattern |
       | Any Search    |
       | any search    |
       | Ugly Code     |
+
+
+    Scenario: List of Gherkin Statements that need step definitions
+
+      Given I launch a browser
+      Given I navigate to "<url>"
+      Given I click the "<value>" element that has attribute id = "<value>"
+      Given I click the "<value>" element that has attribute class = "<value>"
+      Given I click the "<value>" element that has a value of "<value>"
+      Given I click the "<value>" element that has attribute id = "<value>" and a value of "<value>"
+      Given I click the "<value>" element that has attribute
+      Given I enter "<value>" into the "<value>" element that has attribute id = "<value>"
+      Given I enter "<value>" into the "<value>" element that has attribute class = "<value>"
